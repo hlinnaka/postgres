6c195797e47a7e0bc5d2f45bb3d8ba056a4331cb
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2024-07-23 20:21:54 +0300

    Add tests for errors during SSL or GSSAPI handshake
    These test that libpq correctly falls back to a plaintext connection
    on handshake error, in the "prefer" modes.

--- src/interfaces/libpq/t/005_negotiate_encryption.pl
+++ src/interfaces/libpq/t/005_negotiate_encryption.pl
@@ -337,6 +337,15 @@ nossluser   .            disable      postgres       connect, authok            
 			"user=testuser sslmode=prefer",
 			'connect, v2error -> fail');
 		$node->restart;
+
+		$node->safe_psql('postgres',
+						 "SELECT injection_points_attach('backend-ssl-startup', 'error');",
+						 connstr => "user=localuser host=$unixdir");
+		connect_test(
+			$node,
+			"user=testuser sslmode=prefer",
+			'connect, sslaccept, backenderror, reconnect, authok -> plain');
+		$node->restart;
 	}
 
 	# Disable SSL again
@@ -439,6 +448,15 @@ nogssuser   disable      disable      postgres       connect, authok            
 			$node,
 			"user=testuser gssencmode=prefer sslmode=disable",
 			'v2error -> fail');
+		$node->restart;
+
+		$node->safe_psql('postgres',
+						 "SELECT injection_points_attach('backend-gssapi-startup', 'error');",
+						 connstr => "user=localuser host=$unixdir");
+		connect_test(
+			$node,
+			"user=testuser gssencmode=prefer sslmode=disable",
+			'connect, gssaccept, backenderror, reconnect, authok -> plain');
 		$node->restart;
 	}
 }
