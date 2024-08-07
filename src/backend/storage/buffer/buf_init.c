05d4d55419e1f008cd6a37bcda898d87102dd859
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2024-08-07 21:34:25 +0300

    Move per-backend initialization to right place

--- src/backend/storage/buffer/buf_init.c
+++ src/backend/storage/buffer/buf_init.c
@@ -144,10 +144,6 @@ BufferManagerShmemInit(void)
 
 	/* Init other shared buffer-management stuff */
 	StrategyInitialize(!foundDescs);
-
-	/* Initialize per-backend file flush context */
-	WritebackContextInit(&BackendWritebackContext,
-						 &backend_flush_after);
 }
 
 /*
